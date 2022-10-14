fc1ae3f15de844cec3b45f07bedb4184c9094f99
datrudedude <davidelistratov@gmail.com>
2022-10-14 10:46:02 +0300

    Add simple main

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from sample_lib.main import get_post_info
 
 
 if __name__ == '__main__':
+
+    def print_result(post_id):
+        print(get_post_info(post_id))
+
+
     ps = []
     for i in range(0, 90):
-        p = Process(target=get_post_info, args=(i,))
+        p = Process(target=print_result, args=(i,))
         p.start()
         ps.append(p)
     for p in ps:
